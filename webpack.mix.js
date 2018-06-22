e0ddb7b4273f30cb731a38b26a2b791b66312896
Thierry Parent <tparent@absolunet.com>
2018-06-22 08:18:19 -0400

    Add dev-local and watch-local scripts using parent folder as resolved modules path

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,25 +1,8 @@
 const mix = require('laravel-mix');
-const fs = require('fs');
-const path = require('path');
-require('laravel-mix-eslint');
+const utils = require('./webpack.utils');
 
-const distFolderName = 'dist';
-const distFileName = `${__dirname.split(path.sep).pop()}${mix.inProduction() ? '.min' : ''}.js`;
-
-mix.setPublicPath(distFolderName)
-    .webpackConfig({
-        resolve: {
-            modules: [
-                'node_modules',
-                'bower_components'
-            ]
-        }
-    })
+mix.setPublicPath(utils.distFolderName)
+    .webpackConfig(utils.webpackConfig)
     .eslint()
-    .js('index.js', distFileName)
-    .then(() => {
-        const manifestPath = path.resolve(__dirname, distFolderName, 'mix-manifest.json');
-        if (fs.existsSync(manifestPath)) {
-            fs.unlink(manifestPath, () => {});
-        }
-    });
+    .js('index.js', utils.distFileName)
+    .then(utils.removeManifest);
